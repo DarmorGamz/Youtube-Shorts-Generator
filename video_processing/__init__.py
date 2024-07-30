bb6df7f87b022f7c0ac1cce4c5ca3e08abc85ce1
Darren Morrison <darren09@live.ca>
2024-07-29 21:34:35 -0400

    naming convention change

--- video_processing/__init__.py
+++ video_processing/__init__.py
@@ -5,4 +5,4 @@
 from __future__ import absolute_import
 
 # import VideoProccessClient
-from video_proccesing.proccess_client import VideoProccessClient
+from video_processing.process_client import VideoProcessClient
--- video_processing/process_client.py
+++ video_processing/process_client.py
@@ -3,7 +3,7 @@ from moviepy.video.tools.subtitles import SubtitlesClip
 from moviepy.config import change_settings
 change_settings({"IMAGEMAGICK_BINARY": r"C:\\Program Files\\ImageMagick-7.1.1-Q16-HDRI\\magick.exe"})
 
-class VideoProccessClient(object):
+class VideoProcessClient(object):
     def __init__(self):
         pass
 
